ab9bf77788c29cb23d34d5501b1e7af4ff7269c5
wipatrick <patrick.wiener@hs-karlsruhe.de>
2017-03-29 18:13:19 +0200

    fixed paths & changed apk repository for librdkafka

--- base/build.sh
+++ base/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-docker build -t "smueller18/base:anaconda3" ./base/anaconda3
-docker build -t "smueller18/base:python3-alpine" ./base/python3-alpine
-docker build -t "smueller18/base:python3-kafka" ./base/python3-kafka
+docker build -t "smueller18/base:anaconda3" ./anaconda3
+docker build -t "smueller18/base:python3-alpine" ./python3-alpine
+docker build -t "smueller18/base:python3-kafka" ./python3-kafka
 
 # do not build because it is not used
 # docker build -t "smueller18/base:nginx" ./nginx
