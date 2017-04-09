534103800175ffb695899a216bb6077d9726939a
Stephan Müller <mail@stephanmueller.eu>
2017-04-10 00:05:42 +0200

    add kafka topics web app

--- static_html/kafka-topics/js/kafka-topics.js
+++ static_html/kafka-topics/js/kafka-topics.js
@@ -1,15 +1,51 @@
+SCHEMA_REGISTRY_URL = "/schema-registry";
 
-window.onload = function() {
-  var jqxhr = $.get( "example.php", function() {
-    alert( "success" );
+var schemas = {};
+var topics = {};
+
+
+$.get(SCHEMA_REGISTRY_URL + "/subjects", function (subjects) {
+
+    var finishedRequests = 0;
+
+    subjects.forEach(function (subject) {
+
+        $.get(SCHEMA_REGISTRY_URL + "/subjects/" + subject + "/versions/latest", function (schema) {
+
+            var identifier = "";
+
+            if (schema.subject.endsWith("-key")) {
+                topic_name = schema.subject.slice(0, -4);
+                if (typeof(topics[topic_name]) === "undefined")
+                    topics[topic_name] = {};
+
+                topics[topic_name].key = schema.schema;
+            }
+
+            else if (schema.subject.endsWith("-value")) {
+                topic_name = schema.subject.slice(0, -6);
+                if (typeof(topics[topic_name]) === "undefined")
+                    topics[topic_name] = {};
+
+                topics[topic_name].value = schema.schema;
+            }
+
+        }).done(function(){
+            finishedRequests++;
+            if(finishedRequests === subjects.length){
+                printHtml();
+            }
+        })
     })
-    .done(function() {
-      alert( "second success" );
-    })
-    .fail(function() {
-      alert( "error" );
-    })
-    .always(function() {
-      alert( "finished" );
-  });
+});
+
+function printHtml(){
+
+    Object.keys(topics).sort().forEach(function (topic) {
+        $("#content").append($("<h2>").text(topic));
+        $("#content").append($("<h3>").text("Key"));
+        $("#content").append($("<pre>").html(library.json.prettyPrint($.parseJSON(topics[topic].key), null, 4)));
+        $("#content").append($("<h3>").text("Value"));
+        $("#content").append($("<pre>").html(library.json.prettyPrint($.parseJSON(topics[topic].value), null, 4)));
+    });
 }
