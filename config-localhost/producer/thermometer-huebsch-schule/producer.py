7ee69af0b9ca224cdc738d118516229355c47687
Adrian Buerger <adrian.buerger@hs-karlsruhe.de>
2017-05-03 08:34:58 +0200

    adding dummy for producer-thermometer-huebsch-schule

---
+++ config-localhost/producer/thermometer-huebsch-schule/producer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# sorry, the original code is not available for opensource, therefore this placeholder is here
+
+import time
+
+while True:
+    time.sleep(60 * 60)
