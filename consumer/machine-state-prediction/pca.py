1c26411e43f627656096b6652a87945f6376a475
smueller18 <mail@stephanmueller.eu>
2017-07-24 19:00:02 +0200

    add consumer machine-state-prediction

---
+++ consumer/machine-state-prediction/pca.py
@@ -0,0 +1,64 @@
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+
+
+class PCAForPandas(PCA):
+    """This class is just a small wrapper around the PCA estimator of sklearn including normalization to make it compatible with pandas."""
+
+    def __init__(self, **kwargs):
+        self._z_scaler = StandardScaler()
+        super().__init__(**kwargs)
+
+        self._X_columns = None
+
+    def fit(self, X, y=None):
+        """Normalize X and call the fit method of the base class with numpy arrays instead of pandas data frames."""
+
+        X = self._prepare(X)
+
+        self._z_scaler.fit(X.values, y)
+        z_data = self._z_scaler.transform(X.values, y)
+
+        return super().fit(z_data, y)
+
+    def fit_transform(self, X, y=None):
+        """Call the fit and the transform method of this class."""
+
+        X = self._prepare(X)
+
+        self.fit(X, y)
+        return self.transform(X, y)
+
+    def transform(self, X, y=None):
+        """Normalize X and call the transform method of the base class with numpy arrays instead of pandas data frames."""
+
+        X = self._prepare(X)
+
+        z_data = self._z_scaler.transform(X.values, y)
+
+        transformed_ndarray = super().transform(z_data)
+
+        pandas_df = pd.DataFrame(transformed_ndarray)
+        pandas_df.columns = ["pca_{}".format(i) for i in range(len(pandas_df.columns))]
+
+        return pandas_df
+
+    def _prepare(self, X):
+        """Check if the data is a pandas DataFrame and sorts the column names.
+
+        @AttributeError: pandas is not a DataFrame
+                        The columns of the new X is not compatible with the columns from the previous X data
+        """
+        if not isinstance(X, pd.DataFrame):
+            raise AttributeError("X is not a pandas DataFrame")
+
+        X.sort_index(axis=1, inplace=True)
+
+        if self._X_columns is not None:
+            if self._X_columns != list(X.columns):
+                raise AttributeError("The columns of the new X is not compatible with the columns from the previous X data")
+        else:
+            self._X_columns = list(X.columns)
+
+        return X
