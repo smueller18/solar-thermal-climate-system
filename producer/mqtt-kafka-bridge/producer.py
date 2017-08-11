3601e2c0038b72a40add372ee5ab5403fe3a3572
Adrian Buerger <adrian.buerger@hs-karlsruhe.de>
2017-08-11 16:31:31 +0200

    comitting wip for mqtt kafka bridge

--- producer/mqtt-kafka-bridge/producer.py
+++ producer/mqtt-kafka-bridge/producer.py
@@ -4,11 +4,14 @@
 import os
 import logging
 import datetime
+from glob import glob
+
+import paho.mqtt.client as mqtt
 
 from kafka_connector.avro_loop_producer import AvroLoopProducer
 
-__author__ = u'Stephan Müller'
-__copyright__ = u'2017, Stephan Müller'
+__author__ = u'Stephan Müller, Adrian Bürger'
+__copyright__ = u'2017, Stephan Müller, Adrian Bürger'
 __license__ = u'MIT'
 
 __dirname__ = os.path.dirname(os.path.abspath(__file__))
@@ -16,26 +19,55 @@ __dirname__ = os.path.dirname(os.path.abspath(__file__))
 KAFKA_HOSTS = os.getenv("KAFKA_HOSTS", "kafka:9092")
 SCHEMA_REGISTRY_URL = os.getenv("SCHEMA_REGISTRY_URL", "http://schema-registry:8082")
 MQTT_HOST = os.getenv("MQTT_HOST", "mosquitto:1883")
-MQTT_TOPIC = os.getenv("MQTT_TOPIC", "mqtt/topic")
-PRODUCER_TOPIC = os.getenv("PRODUCER_TOPIC", "prod.stcs.???")
 
-LOGGING_LEVEL = os.getenv("LOGGING_LEVEL", "INFO")
-logging_format = "%(levelname)8s %(asctime)s %(name)s [%(filename)s:%(lineno)s - %(funcName)s() ] %(message)s"
-
-# TODO schema for every topic
-key_schema = __dirname__ + "/config/key.avsc"
-value_schema = __dirname__ + "/config/value.avsc"
-
-logging.basicConfig(level=logging.getLevelName(LOGGING_LEVEL), format=logging_format)
-logger = logging.getLogger('producer')
+# MQTT_TOPIC = os.getenv("MQTT_TOPIC", "mqtt/topic")
 
 
-producer = AvroLoopProducer(KAFKA_HOSTS, SCHEMA_REGISTRY_URL, PRODUCER_TOPIC, key_schema, value_schema)
+# Collecto the name of all folders within the "config" directory, which
+# corresponds to the names of all topics that we want to bridge Collect all the topics we want to bridge, and initialize according producers
 
-# TODO listening to mosquitto and producing to Kafka
-while True:
+mqtt_topics = glob("*")
 
-    key = {'timestamp': int(time.time() * 1000)}
-    value = {'value': 1}
+producers = {}
 
-    producer.produce(key, value)
+for topic in mqtt_topics:
+
+    PRODUCER_TOPIC = os.getenv("PRODUCER_TOPIC", "prod.stcs." + (topic))
+
+    LOGGING_LEVEL = os.getenv("LOGGING_LEVEL", "INFO")
+    logging_format = "%(levelname)8s %(asctime)s %(name)s [%(filename)s:%(lineno)s - %(funcName)s() ] %(message)s"
+
+    # TODO schema for every topic
+    key_schema = os.path.join([__dirname__, "config", topic, "key.avsc"])
+    value_schema = os.path.join([__dirname__, "config", topic, "value.avsc"])
+
+    logging.basicConfig(level=logging.getLevelName(LOGGING_LEVEL), format=logging_format)
+    logger = logging.getLogger('mqtt_kafka_bridge_topic_' + topic)
+
+    producer = AvroLoopProducer(KAFKA_HOSTS, SCHEMA_REGISTRY_URL, PRODUCER_TOPIC, key_schema, value_schema)
+
+    producers[topic] = producer
+
+
+def on_connect(client, userdata, flags, rc):
+
+    print("Connected with result code "+str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("prod\stcs\+")
+
+
+def on_message(client, userdata, msg):
+
+    # her comes the publishing
+
+    print(msg.topic+" "+str(msg.payload))
+
+
+
+mqtt_client = mqtt.Client()
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.loop_forever()
