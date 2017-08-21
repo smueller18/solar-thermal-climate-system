ce1faf032a1bda5b57929d74e63ff8163a89bf3e
Adrian Buerger <adrian.buerger@hs-karlsruhe.de>
2017-08-21 12:42:55 +0200

    mqtt python3 container

--- docker/build.sh
+++ docker/build.sh
@@ -3,3 +3,4 @@
 docker build -t "smueller18/stcs:python2" ./python2
 docker build -t "smueller18/stcs:python3" ./python3
 docker build -t "smueller18/stcs:anaconda3" ./anaconda3
+docker build -t "smueller18/stcs:python3-mqtt" ./python3-mqtt
